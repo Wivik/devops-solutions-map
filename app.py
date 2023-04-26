74eb6823555cfde6e87d925f21a62f45538fc740
Wivik <48727868+Wivik@users.noreply.github.com>
2023-04-26 21:43:57 +0200

    feat: static site generator mode :sparkles: closes #3

--- app.py
+++ app.py
@@ -7,12 +7,20 @@ import pathlib
 import sys
 import yaml
 import json
+from flask_frozen import Freezer
+import argparse
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(32)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 csrf = CSRFProtect()
 csrf.init_app(app)
+freezer = Freezer(app)
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('-f', '--freeze', dest='freeze_mode', action='store_true', default=False, help='Produce a static website instead of starting the web server')
+arg_parser.add_argument('-d', '--debug', dest='debug', action='store_true', default=False, help='Enable Flask debug mode')
+args = arg_parser.parse_args()
 
 ## solutions file is expected to be an env var
 ## defaulting to solutions.yaml
@@ -29,12 +37,10 @@ solutions_file = os.path.join(data_path, solutions_file_name)
 if not os.path.exists(data_path):
     print('Data path missing, creating directory')
     try:
-        os.path.mkdir(data_path)
+        os.mkdir(data_path)
     except:
         print('Could not create data dir')
         raise
-
-
 
 ## here we go
 
@@ -94,7 +100,7 @@ def main(side=None, step=None):
 
     return render_template('main.html.j2', devops_content=devops_content)
 
-@app.route("/tool/<string:tool>")
+@app.route("/tool/<string:tool>.html")
 def tool_view(tool):
 
     if not test_solutions_file(solutions_file):
@@ -111,7 +117,7 @@ def tool_view(tool):
 
     return render_template('tool.html.j2', tool_content=tool_content, tool_usage=tool_usage)
 
-@app.route("/about")
+@app.route("/about.html")
 def about():
 
     changelog = open(os.path.join(app_path, 'CHANGELOG.md'), 'r')
@@ -125,3 +131,9 @@ def about():
     version = str(version['version'])
 
     return render_template('about.html.j2', changelog_content=changelog_content, version=version, data_spec=data_spec)
+
+if __name__ == '__main__':
+    if args.freeze_mode:
+        freezer.freeze()
+    else:
+        Flask.run(app, debug=args.debug)
